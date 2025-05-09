68c1a5994d89b67bd8b5cacd41d6cab8993c08b0
rads-online <contactradsonline@gmail.com>
2025-05-10 01:06:03 +0530

    vercel problem fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+plugins: [react()],
+build: {
+rollupOptions: {
+external: [
+'firebase/app',
+'firebase/auth',
+'firebase/firestore',
+'firebase/analytics',
+'firebase/storage'
+]
+}
+}
+});
